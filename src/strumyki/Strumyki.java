43b9684c31337b5bd490e6d209e4038cd0c8f6ef
master0612 <yska1994@gmail.com>
2016-04-16 16:19:27 +0200

    commiter

--- src/strumyki/Strumyki.java
+++ src/strumyki/Strumyki.java
@@ -20,7 +20,21 @@ public class Strumyki {
      */
     public static void main(String[] args)  {
          
-        System.out.println("Ha!");
-        int dupsko=0;
+        for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");for(int i = 0 ; i<20;i++)
+            System.out.println("dupsko");
+                      
     }
 }
