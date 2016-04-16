c95c6be2207a472a803193278a57284196699ba7
master0612 <yska1994@gmail.com>
2016-04-16 22:50:19 +0200

    commiter

--- src/strumyki/Strumyki.java
+++ src/strumyki/Strumyki.java
@@ -16,17 +16,11 @@ public class Strumyki {
     /**
      * @param args the command line arguments
      */
-    class odbyt
-    {
-        String dupa = "ahahahah";
-    }
+    
     public static void main(String[] args)  {
-        int d;
-      if( (d=0) == 0)
-      {
-          for(int chujowie=0;chujowie<10;chujowie++)
-            System.out.println("hoohohoh");
-      }
+        int tab[] = new int[5];
+        tab[5]=60;
+        int d = tab[22];
   
 }
 }
